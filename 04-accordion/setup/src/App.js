40c4fc7892667b47d0ec2caca9afd87b9bd0cd9a
= <atharvarsalokhe2@gmail.com>
2022-02-06 22:35:57 +0530

    final commit

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const questions = data;
+
+  return (
+    <main>
+      <div className="container">
+        <h3>Question And Answers About Login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <article className="question" key={question.id}>
+                <SingleQuestion {...question} />
+              </article>
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <>
+      <header>
+        <h4>{title}</h4>
+        {/* toggle functionality! */}
+        <button className="btn" onClick={() => setReadMore(!readMore)}>
+          {readMore ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      <p>{readMore ? `${info}` : ""}</p>
+    </>
+  );
 };
 
 export default Question;
